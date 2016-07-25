3ea75ef550bcd07f922d5618e6f8d4611d7f8c0d
Danil Vnukov <freezer314@gmail.com>
2016-07-25 15:26:34 +0300

    First Upload

---
+++ js/common.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+ 
+  $(".owl-carousel").owlCarousel({
+ 
+      items: 1,
+      lazyLoad: true,
+      autoPlay: true,
+      navigation: true,
+      navigationText: false,
+      pagination: false,
+ 
+  });
+ 
+});
